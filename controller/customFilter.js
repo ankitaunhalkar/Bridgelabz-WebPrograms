fc3003d11563f090a7b44bd3be437320d6726a2c
bridgeit <ankitaunhalkar19@gmail.com>
2018-04-30 19:27:04 +0530

    commit

--- controller/customFilter.js
+++ controller/customFilter.js
@@ -1,13 +1,55 @@
 app.filter('selectedTags', function() {
 // filter to check array of elements
-return function(users, tags) {
-   return users.filter(function(user) {
-       if (tags.indexOf(user.specs.manufacturer) != -1) {
-           return true;
-       } else if(tags.length == 0) {
-           return true;
-       }
-       return false;
-   });
+return function(users, tags)
+{
+  //Json data - users
+  // tags- selected data
+     if (users !== undefined) {
+       return users.filter(function(product) {
+         if (tags.indexOf(product.specs.manufacturer) != -1) {
+             return true;
+         }
+         else if (tags.indexOf(product.specs.storage) != -1) {
+             return true;
+         }
+         else if (tags.indexOf(product.specs.os) != -1) {
+             return true;
+         }
+         else if (tags.indexOf(product.specs.camera) != -1) {
+             return true;
+         }
+         else if(tags.length == 0) {
+             return true;
+         }
+         return false;
+     });
+     }
 };
 });
+
+
+// app.filter('selectedTags', function() {
+// // filter to check array of elements
+// return function(jsonData, manufacturerList) {
+//
+//    var returnArray=[];
+//    var completeArray=[];
+//   if(jsonData !=undefined)
+//   {
+//     var product = jsonData[i];
+//     if(manufacturerList != undefined)
+//     {
+//       for(var i=0;i<jsonData.length;i++)
+//       {
+//         for(var j=0;j<manufacturerList.length;j++)
+//         {
+//           if(product.specs.manufacturer==manufacturerList[j])
+//           {
+//               returnArray.push(product.specs.manufacturer);
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
+// });
--- controller/homeController.js
+++ controller/homeController.js
@@ -19,13 +19,26 @@ $scope.login = function () {
   // selected manufacturer
 $scope.selected = [];
 // whole manufacturer
-$scope.manufacturer = [];
+var manufacturer = [];
+var storage = [];
+var os = [];
+var camera = [];
 
 // when user clicks on checkbox, change selected list
 $scope.toggle = function (item, list) {
+  console.log("item  ",item);
+
   var idx = list.indexOf(item);
-  if (idx > -1) list.splice(idx, 1);
-  else list.push(item);
+    if (idx > -1)
+    {
+      list.splice(idx, 1);
+    }
+    // console.log("dsfhfgdh  ",list);
+  else
+  {
+    list.push(item);
+  }
+  console.log("list  ",list);
 };
 
 // is item exists in list
@@ -34,10 +47,21 @@ $scope.exists = function (item, list) {
 };
 
 // process user data and prepare whole places
-angular.forEach($scope.jsonData, function(user, key) {
-   if($scope.specs.manufacturer.indexOf(user.specs.manufacturer) == -1) {
-       $scope.specs.manufacturer.push(user.specs.manufacturer);
-   }
-});
-
+// angular.forEach($scope.jsonData, function(user, key) {
+//    if($scope.specs.manufacturer.indexOf(user.specs.manufacturer) == -1) {
+//        $scope.specs.manufacturer.push(user.specs.manufacturer);
+//    }
+//    else if($scope.specs.storage.indexOf(user.specs.storage) == -1) {
+//        $scope.specs.storage.push(user.specs.storage);
+//    }
+//    else if($scope.specs.os.indexOf(user.specs.os) == -1) {
+//        $scope.specs.os.push(user.specs.os);
+//    }
+//    else if($scope.specs.os.indexOf(user.specs.os) == -1) {
+//        $scope.specs.os.push(user.specs.os);
+//    }
+//    else if($scope.specs.camera.indexOf(user.specs.camera) == -1) {
+//        $scope.specs.camera.push(user.specs.camera);
+//    }
+// });
 });
